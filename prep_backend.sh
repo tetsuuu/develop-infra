e9488cb0f82a4c509fbcce2ce620c1ee67ba34a6
uluru-kinoshitat <t_kinoshita@uluru.jp>
2019-09-24 04:12:11 +0900

    fix prep_backend.sh

--- prep_backend.sh
+++ prep_backend.sh
@@ -2,7 +2,7 @@
 ## ACCOUNT = service name
 ## BUCKET = uluru-enphoto-infra-tfstate
 ## STAGE = develop or staging or production ( or sandbox )
-## e.g.) ../prep_backend.sh -a enphoto -b uluru-enphoto-infra-tfstate -s develop > terraform.tf
+## e.g.) ../prep_backend.sh -a enphoto -b uluru-enphoto-infra-tfstate -r cloudfront -s develop > terraform.tf
 ## S3 PATH : s3://uluru-enphoto-infra-tfstate/terraform/enphoto/develop/terraform.tfstate
 
 init() {
@@ -12,7 +12,7 @@ init() {
 terraform {
   backend "s3" {
     bucket  = "${BUCKET}"
-    key     = "terraform/${ACCOUNT}/${STAGE}/terraform.tfstate"
+    key     = "terraform/${ACCOUNT}/${STAGE}/${RESOURCE}/terraform.tfstate"
     region  = "ap-northeast-1"
   }
 }
@@ -20,13 +20,16 @@ EOS
 
 }
 
-while getopts "b:s:a:" opt; do
+while getopts "b:s:r:a:" opt; do
   case "$opt" in
     a)
       ACCOUNT=$OPTARG
       ;;
     b)
       BUCKET=$OPTARG
+      ;;
+    r)
+      RESOURCE=$OPTARG
       ;;
     s)
       STAGE=$OPTARG
